b0b47139cadb90b953e98f3b81ee5f7f834aa815
Orphée TOHOU <orpheetohou16@gmail.com>
2023-01-29 13:22:45 +0100

    Création du 4eme Bouton

---
+++ src/components/buttons/Bouton4.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Bouton4 = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <button className="border-solid  border-2 border-blue-600 bg-blue-600 py-3 px-20 text-white  font-Mukta text-lg rounded-lg hover:transition duration-500 ease-in-out hover:bg-white hover:border-blue-600 hover:text-blue-600">
+        En savoir plus 
+      </button>
+    </div>
+  )
+}
+
+export default Bouton4;
