f9c58b51dd8adcb142853977b863b875445cfc15
Orphée TOHOU <orpheetohou16@gmail.com>
2023-01-29 14:05:57 +0100

    Création du 5eme Bouton

---
+++ src/components/buttons/Bouton5.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Bouton5 = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <button className=" border-solid uppercase border-2 border-red-600 p-3 bg-red-600 font-serif text-white text-lg rounded-lg hover:bg-red-800 ">
+        Danger
+      </button>
+    </div>
+  )
+}
+
+export default Bouton5;
